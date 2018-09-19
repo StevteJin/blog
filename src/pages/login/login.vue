a890484e312976d842f228a713cff0564b495a8b
金 <1211050148@qq.com>
2018-09-19 18:11:12 +0800

    登录

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,191 +1,135 @@
 <template>
-  <div>
-    <navbar :title="title"></navbar>
-    <div class="contentbox" v-if="realySignup==false">
-      <input id="1" type="text" placeholder="请输入用户名或绑定的手机号" style="display:none;">
-      <input id="2" type="password" placeholder="请输入密码" style="display:none;">
-      <input class="input" type="text" placeholder="请输入用户名或绑定的手机号" v-model="username">
-      <input class="input" type="password" placeholder="请输入密码" v-model="password">
-      <div class="remember">
-        <div class="remember_text">
-          <input id="password" class="rdo" type="checkbox" value="5"><label for="password">自动登录</label>
-        </div>
-        <div class="remember_text forget_password" @click="toForgetPassword">忘记密码</div>
-      </div>
-      <div class="login" @click="loginNow">登录</div>
-      <div class="to_signup">还没有账号？立即<span @click="toSignup">注册</span></div>
-      <div class="line"><span>第三方登录</span></div>
-    </div>
-    <div class="contentbox" v-if="realySignup==true" style="font-size:20px;">恭喜您登录成功，移动站正在开发中</div>
-  </div>
+  <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card>
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>登录</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <!--这里为了防止浏览器自动填充表单 s-->
+                  <input type="text" style="display:none;" name="username">
+                  <input type="password" style="display:none;" name="password">
+                  <!--这里为了防止浏览器自动填充表单 e-->
+                  <v-text-field prepend-icon="person" label="用户名或者手机号" type="text" v-model="username" ref="username" :rules="[rules.required, rules.counter]"></v-text-field>
+                  <v-text-field prepend-icon="lock" label="密码" type="password" v-model="password" ref="password" :rules="[rules.required, rules.counter]"></v-text-field>
+                  <v-layout row justify-space-between>
+                    <v-text-field prepend-icon="insert_photo" label="图形证码" type="text"  v-model="validation" ref="validation" :rules="[rules.required]"></v-text-field>
+                    <v-btn block depressed class="button-right" @click="picturevalidation"><img :src="imgValidation" alt="" @click="picturevalidation"></v-btn>
+                  </v-layout>
+                </v-form>
+              </v-card-text>
+              <v-layout>
+                <v-btn color="primary" block class="regist" @click="submit">登录</v-btn>
+              </v-layout>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import navbar from '@/components/navbar.vue';
-
 export default {
-  components: {
-    navbar
-  },
   data: function () {
     return {
-      title: '登录',
       username: '',
       password: '',
-      realySignup: false
+      password1: '',
+      mobile: '',
+      validation: '',
+      rules: {
+        required: value => !!value || '内容不能为空',
+        counter: value => value.length <= 12 || '长度不得超过12位'
+      },
+      second: '获取验证码',
+      validationShow: false,
+      count: 70,
+      imgValidation: '',
+      formHasErrors: false
     };
   },
-  mounted: function () {},
+  created: function () {
+    this.picturevalidation();
+  },
+  computed: {
+    form () {
+      return {
+        username: this.username,
+        password: this.password,
+        validation: this.validation
+      };
+    }
+  },
   methods: {
-    loginNow: function () {
-      this.axios
-        .post('/login', {
-          user_name: this.username,
-          password: this.password
-        })
-        .then(response => {
-          console.log(typeof (response.data.code));
-          if (response.data.code === 100) {
-            this.realySignup = true;
-          } else {
-            this.$toast(response.data.msg);
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    },
-    toForgetPassword: function () {
-      this.$router.push({
-        path: '/forgetpassword'
+    picturevalidation: function () {
+      // this.axios
+      //   .get('/getPicture')
+      //   .then(response => {
+      //     this.imgValidation = response.data.data;
+      //   });
+      let that = this;
+      const instance = this.axios.create({
+        url: '/getPicture',
+        method: 'get',
+        transformResponse: [function (data) {
+          // 对 data 进行任意转换处理
+          // console.log(typeof (data));
+          that.imgValidation = JSON.parse(data).data;
+          console.log(that.imgValidation);
+        }],
+        // 前后端分离请求要携带cookie必须带这个
+        withCredentials: true
       });
+      instance();
     },
-    toSignup: function () {
-      this.$router.push({
-        path: '/signup'
+    submit () {
+      console.log(222);
+      this.formHasErrors = false;
+      Object.keys(this.form).forEach(f => {
+        console.log(f);
+        if (!this.form[f]) {
+          this.formHasErrors = true;
+          this.$refs[f].validate(true);
+        } else {
+          let url = '/login';
+          const params = {
+            user_name: this.username,
+            password: this.password,
+            validation: this.validation
+          };
+          this.axios
+            .post(url, params)
+            .then(response => {
+              // console.log(JSON.stringify(response.data));
+              if (response.data.code === 100) {
+                this.$toast('恭喜您注册成功，移动站正在开发中，敬请期待');
+              } else {
+                this.$toast(response.data.msg);
+              }
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        };
       });
     }
   }
 };
 </script>
 
-<style lang="scss" scoped>
-  .contentbox {
-    display: flex;
-    justify-content: center;
-    flex:0 0 375px;
-    flex-wrap:wrap;
-  }
-
-  .logo {
-    flex: 0 0 285px;
-    height: 70px;
-    margin-top: 50px;
-    margin-bottom:60px;
-  }
-  .input {
-    flex: 0 0 285px;
-    height: 40px;
-    margin-top: 14px;
-    background-color:#F6F5F5;
-    color:#D6D6D6;
-    border-radius: 20px;
-    padding-left:16px;
-    line-height:40px;
-    font-size:12px;
-    box-sizing: border-box;
-  }
-  .input::-webkit-input-placeholder {
-    color: #D6D6D6;
-    font-size: 12px;
-  }
-  .remember{
-    flex: 0 0 285px;
-    height: 40px;
-    display: flex;
-    justify-content: space-between;
-  }
-  .remember .remember_text{
-    flex:0 0 50%;
-    height:40px;
-    line-height:40px;
-  }
-  .remember .forget_password{
-    color:#C80E42;
-    text-decoration: underline;
-    text-align: right;
-  }
-  .remember .remember_text input{
-    width:15px;
-    height:15px;
-    display: inline-block;
-    box-sizing: border-box;
-    vertical-align:middle;
-  }
-  .remember .remember_text label{
-    display: inline-block;
-    margin-left:4px;
-    line-height:40px;
-    font-size:13px;
-    color:#666666;
-    vertical-align:middle;
-  }
-  .login {
-    flex: 0 0 285px;
-    height: 40px;
-    background-color:#258CE3;
-    color:#FFFFFF;
-    border-radius: 20px;
-    line-height:40px;
-    font-size:12px;
-    box-sizing: border-box;
-    font-size:13px;
-    text-align: center;
-  }
-  .line{
-    flex: 0 0 285px;
-    height: 40px;
-    line-height:40px;
-    display: flex;
-    justify-content: space-between;
-    position: relative;
-  }
-  .line:before{
-    height:1px;
-    background-color:#D9D9D9;
-    content: '';
-    width: 33.33%;
-    position: absolute;
-    top:49%;
-    left:0;
-  }
-  .line:after{
-    height:1px;
-    background-color:#D9D9D9;
-    content: '';
-    width:33.33%;
-    position: absolute;
-    top:49%;
-    right:0;
-  }
-  .line span{
-    width:33.33%;
-    position: absolute;
-    left:33.33%;
-    text-align: center;
-    font-size:12px;
-    color:#999999;
-  }
-  .to_signup{
-    flex: 0 0 285px;
-    height: 40px;
-    text-align: center;
-    color:#D6D6D6;
-    font-size:12px;
-    line-height:40px;
-  }
-  .to_signup span{
-    color:#C80E42;
-    text-decoration: underline;
-  }
+<style scoped>
+.button-right {
+  height: 43px;
+  margin-left: 10px;
+}
+.regist {
+  margin-left: 18px;
+  margin-right: 18px;
+  margin-bottom:40px;
+}
 </style>
