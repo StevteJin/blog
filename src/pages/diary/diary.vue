7fb8d1d4286bdbb818149b48672a35c4f6e24218
金 <1211050148@qq.com>
2018-09-30 09:48:55 +0800

    去标签云

--- src/pages/diary/diary.vue
+++ src/pages/diary/diary.vue
@@ -20,12 +20,6 @@
             <li v-for="(item,index) in hotRead" :key="index"><a href="/">{{item.title}}</a></li>
           </ul>
         </div>
-        <div class="cloud">
-          <h2>标签云</h2>
-            <ul>
-              <a href="/">陌上花开</a> <a href="/">校园生活</a> <a href="/">html5</a> <a href="/">SumSung</a> <a href="/">青春</a> <a href="/">温暖</a> <a href="/">阳光</a> <a href="/">三星</a><a href="/">索尼</a> <a href="/">华维荣耀</a> <a href="/">三星</a> <a href="/">索尼</a>
-            </ul>
-        </div>
         <guanzhu></guanzhu>
       </aside>
       <main class="r_box">
